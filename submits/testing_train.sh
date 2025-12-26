f3d11cc5689dff4bf4db9e34ef5c8eb7456e6cb0
clara.christiansen <s214659@dtu.dk>
2025-12-26 10:31:37 +0100

    Added wandb args in .env

--- submits/testing_train.sh
+++ submits/testing_train.sh
@@ -9,9 +9,9 @@
 ### -- specify that the cores must be on the same host -- 
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- specify that we need 4GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "rusage[mem=2GB]"
 ### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
-#BSUB -M 2GB
+#BSUB -M 3GB
 ### -- set walltime limit: hh:mm -- 
 #BSUB -W 12:00 
 ### -- set the email address -- 
@@ -24,18 +24,32 @@
 #BSUB -N 
 ### -- Specify the output and error file. %J is the job-id -- 
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o train_function_98_%J.out 
-#BSUB -e train_function_98_%J.err 
+#BSUB -o /work3/s214659/ML4HD/ML4HD_project/submits/outputs/train_function_98_%J.out
+#BSUB -e /work3/s214659/ML4HD/ML4HD_project/submits/outputs/train_function_98_%J.err
 
-# here follow the commands you want to execute with input.in as the input file
+### Conda and cwd
 cd /work3/s214659/ML4HD/ML4HD_project || exit 1
 source /work3/s214659/miniconda3/etc/profile.d/conda.sh
 conda activate ML4HD
 
+### Check TF
 python - << 'EOF'
 import tensorflow as tf
 print("TF version:", tf.__version__)
 print("GPUs:", tf.config.list_physical_devices("GPU"))
 EOF
 
-python -m src.train --use_wandb --wandb_run_name "test_98"
+### Get wandb settings
+set -a
+. config/.env
+set +a
+
+ARCHITECTURE="cnn_tpool2"
+FRAMES=98
+
+### Run
+python -m src.train --use_wandb \
+                    --wandb_run_name "train_${ARCHITECTURE}_frames${FRAMES}_${LSB_JOBID}" \
+                    --architecture ${ARCHITECTURE} \
+                    --epochs 50 \
+                    --frames ${FRAMES}
